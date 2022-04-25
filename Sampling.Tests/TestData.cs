2810889ea7d5f987a788a4b6bae00d06adbcba0b
Thomas Radigk <t.radigk@outlook.de>
2022-04-25 17:04:17 +0200

    More tests with extracted test data

---
+++ Sampling.Tests/TestData.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using Sampling.Enumerations;
+using Sampling.Models;
+
+namespace Sampling.Tests;
+
+internal class TestData
+{
+    public static DateTime NewTestData1StartTimeStamp = DateTime.Parse("2017-01-03T01:05:00");
+
+    public static readonly List<Measurement> NewTestData1 = new()
+                                                            {
+                                                                new Measurement(DateTime.Parse("2016-01-03T10:04:45"),
+                                                                                35.79,
+                                                                                MeasurementType.Temp),
+                                                                new Measurement(DateTime.Parse("2017-01-03T10:09:07"),
+                                                                                35.01,
+                                                                                MeasurementType.Temp),
+                                                                new Measurement(DateTime.Parse("2017-01-03T10:01:18"),
+                                                                                98.78,
+                                                                                MeasurementType.SpO2),
+                                                                new Measurement(DateTime.Parse("2017-01-03T10:02:01"),
+                                                                                35.82,
+                                                                                MeasurementType.Temp),
+                                                                new Measurement(DateTime.Parse("2017-01-03T10:05:00"),
+                                                                                97.17,
+                                                                                MeasurementType.SpO2),
+                                                                new Measurement(DateTime.Parse("2016-01-03T10:05:01"),
+                                                                                95.08,
+                                                                                MeasurementType.SpO2),
+                                                                new Measurement(DateTime.Parse("2017-03-05T06:45:12"),
+                                                                                91.01,
+                                                                                MeasurementType.SpO2),
+                                                                new Measurement(DateTime.Parse("2017-01-03T10:03:34"),
+                                                                                96.49,
+                                                                                MeasurementType.SpO2),
+                                                                new Measurement(DateTime.Parse("2017-01-02T10:05:06"),
+                                                                                98.16,
+                                                                                MeasurementType.SpO2),
+                                                                new Measurement(DateTime.Parse("2017-01-04T04:16:27"),
+                                                                                36.12,
+                                                                                MeasurementType.Temp),
+                                                                new Measurement(DateTime.Parse("2017-01-03T03:53:01"),
+                                                                                34.98,
+                                                                                MeasurementType.Temp),
+                                                                new Measurement(DateTime.Parse("2017-01-03T16:37:31"),
+                                                                                89.11,
+                                                                                MeasurementType.SpO2)
+                                                            };
+
+    public static readonly Dictionary<MeasurementType, List<Measurement>> ExpectedNewTestData1Result = new()
+        {
+            {
+                MeasurementType.Temp, new List<Measurement>
+                                      {
+                                          new(DateTime.Parse("2017-01-03T03:55:00"), 34.98, MeasurementType.Temp),
+                                          new(DateTime.Parse("2017-01-03T10:05:00"), 35.82, MeasurementType.Temp),
+                                          new(DateTime.Parse("2017-01-03T10:10:00"), 35.01, MeasurementType.Temp),
+                                          new(DateTime.Parse("2017-01-04T04:20:00"), 36.12, MeasurementType.Temp),
+                                      }
+            },
+            {
+                MeasurementType.SpO2, new List<Measurement>
+                                      {
+                                          new(DateTime.Parse("2017-01-03T10:05:00"), 97.17, MeasurementType.SpO2),
+                                          new(DateTime.Parse("2017-01-03T16:40:00"), 89.11, MeasurementType.SpO2),
+                                          new(DateTime.Parse("2017-03-05T06:50:00"), 91.01, MeasurementType.SpO2),
+                                      }
+            }
+        };
+}
--- Sampling.Tests/TestOfSampling.cs
+++ Sampling.Tests/TestOfSampling.cs
@@ -11,32 +11,39 @@ namespace Sampling.Tests
     [TestClass]
     public class TestOfSampling
     {
-        private readonly List<Measurement> _testDataSet1 = new()
-                                                           {
-                                                               new Measurement(DateTime.Parse("2017-01-03T10:04:45"),
-                                                                               35.79,
-                                                                               MeasurementType.Temp),
-                                                               new Measurement(DateTime.Parse("2017-01-03T10:01:18"),
-                                                                               98.78,
-                                                                               MeasurementType.SpO2),
-                                                               new Measurement(DateTime.Parse("2017-01-03T10:09:07"),
-                                                                               35.01,
-                                                                               MeasurementType.Temp),
-                                                               new Measurement(DateTime.Parse("2017-01-03T10:03:34"),
-                                                                               96.49,
-                                                                               MeasurementType.SpO2),
-                                                               new Measurement(DateTime.Parse("2017-01-03T10:02:01"),
-                                                                               35.82,
-                                                                               MeasurementType.Temp),
-                                                               new Measurement(DateTime.Parse("2017-01-03T10:05:00"),
-                                                                               97.17,
-                                                                               MeasurementType.SpO2),
-                                                               new Measurement(DateTime.Parse("2017-01-03T10:05:01"),
-                                                                               95.08,
-                                                                               MeasurementType.SpO2)
-                                                           };
+        private readonly List<Measurement> _providedTestsDataSet = new()
+                                                                   {
+                                                                       new Measurement(DateTime
+                                                                            .Parse("2017-01-03T10:04:45"),
+                                                                        35.79,
+                                                                        MeasurementType.Temp),
+                                                                       new Measurement(DateTime
+                                                                            .Parse("2017-01-03T10:01:18"),
+                                                                        98.78,
+                                                                        MeasurementType.SpO2),
+                                                                       new Measurement(DateTime
+                                                                            .Parse("2017-01-03T10:09:07"),
+                                                                        35.01,
+                                                                        MeasurementType.Temp),
+                                                                       new Measurement(DateTime
+                                                                            .Parse("2017-01-03T10:03:34"),
+                                                                        96.49,
+                                                                        MeasurementType.SpO2),
+                                                                       new Measurement(DateTime
+                                                                            .Parse("2017-01-03T10:02:01"),
+                                                                        35.82,
+                                                                        MeasurementType.Temp),
+                                                                       new Measurement(DateTime
+                                                                            .Parse("2017-01-03T10:05:00"),
+                                                                        97.17,
+                                                                        MeasurementType.SpO2),
+                                                                       new Measurement(DateTime
+                                                                            .Parse("2017-01-03T10:05:01"),
+                                                                        95.08,
+                                                                        MeasurementType.SpO2)
+                                                                   };
 
-        private readonly Dictionary<MeasurementType, List<Measurement>> _expectedResultData1 = new()
+        private readonly Dictionary<MeasurementType, List<Measurement>> _expectedResultDataForProvided = new()
             {
                 {
                     MeasurementType.Temp, new List<Measurement>
@@ -55,7 +62,7 @@ namespace Sampling.Tests
             };
 
 
-        private MeasurementSampler _sut;
+        private MeasurementSampler _sut = null!;
 
         [TestInitialize]
         public void TestInitialize()
@@ -66,14 +73,14 @@ namespace Sampling.Tests
         [TestMethod]
         public void TestSampling()
         {
-            var result = _sut.Sample(DateTime.Parse("2017-01-03T10:04:44"), _testDataSet1);
+            var result = _sut.Sample(DateTime.Parse("2017-01-03T10:04:44"), _providedTestsDataSet);
 
             var measurementTypes = Enum.GetValues(typeof(MeasurementType));
             var resultKeys = result.Keys;
 
             foreach (var resultKey in result.Keys)
             {
-                CollectionAssert.AreEqual(_expectedResultData1[resultKey], result[resultKey]);
+                CollectionAssert.AreEqual(_expectedResultDataForProvided[resultKey], result[resultKey]);
             }
 
             CollectionAssert.AreEqual(measurementTypes, resultKeys);
@@ -82,16 +89,16 @@ namespace Sampling.Tests
         [TestMethod]
         public void TestSamplingWithDebugOutput()
         {
-            var result = _sut.Sample(DateTime.Parse("2017-01-03T10:04:44"), _testDataSet1);
+            var result = _sut.Sample(DateTime.Parse("2017-01-03T10:04:44"), _providedTestsDataSet);
 
             var measurementTypes = Enum.GetValues(typeof(MeasurementType));
             var resultKeys = result.Keys;
-            string inputDataString = String.Join($"{Environment.NewLine}", _testDataSet1);
+            string inputDataString = String.Join($"{Environment.NewLine}", _providedTestsDataSet);
             Debug.WriteLine("INPUT:");
             Debug.WriteLine(inputDataString);
             foreach (var resultKey in result.Keys)
             {
-                CollectionAssert.AreEqual(_expectedResultData1[resultKey], result[resultKey]);
+                CollectionAssert.AreEqual(_expectedResultDataForProvided[resultKey], result[resultKey]);
             }
 
             CollectionAssert.AreEqual(measurementTypes, resultKeys);
@@ -104,6 +111,37 @@ namespace Sampling.Tests
             {
                 Debug.WriteLine(outputDataString);
             }
+        }
+
+        [TestMethod]
+        public void TestSamplingWithMoreData()
+        {
+            List<Measurement> newTestData = TestData.NewTestData1;
+            DateTime startOfSampling = TestData.NewTestData1StartTimeStamp;
+
+            var result = _sut.Sample(startOfSampling, newTestData);
+
+            var measurementTypes = Enum.GetValues(typeof(MeasurementType));
+            var resultKeys = result.Keys;
+            string inputDataString = String.Join($"{Environment.NewLine}", newTestData);
+            Debug.WriteLine("INPUT:");
+            Debug.WriteLine(inputDataString);
+
+
+            Debug.WriteLine("OUTPUT:");
+            foreach (string outputDataString in result.Values.Select(sampledOutput =>
+                                                                         String.Join($"{Environment.NewLine}",
+                                                                          sampledOutput)))
+            {
+                Debug.WriteLine(outputDataString);
+            }
+
+            foreach (var resultKey in result.Keys)
+            {
+                CollectionAssert.AreEqual(TestData.ExpectedNewTestData1Result[resultKey], result[resultKey]);
+            }
+
+            CollectionAssert.AreEqual(measurementTypes, resultKeys);
         }
     }
 }
